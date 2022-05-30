8b40a0e2b8a46c4e9c7d49f9c81f70c4ded4068f
Takeshi Kovacs <antonbasket@list.ru>
2022-05-31 01:13:33 +1000

    Add lower bar

--- app/src/main/java/ru/netology/myapp/MainActivity.kt
+++ app/src/main/java/ru/netology/myapp/MainActivity.kt
@@ -2,5 +2,20 @@ package ru.netology.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val likesButton = findViewById<Button>(R.id.likes)
+        val likesFigures = findViewById<TextView>(R.id.likesCount)
+        var likesQuantity = 0
+        likesButton.setOnClickListener {
+            likesQuantity++
+            likesFigures.text = likesQuantity.toString()
+        }
+    }
+}
